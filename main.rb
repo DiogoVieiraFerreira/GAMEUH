771892a2efc2987b576fbc61ba1145a3f274b94b
DiogoVieiraFerreira <Diogo.VIEIRA-FERREIRA@cpnv.ch>
2020-08-28 17:33:22 +0200

    draw map with txt file

--- main.rb
+++ main.rb
@@ -1,8 +1,11 @@
+require 'pp'
 require 'gosu'
+require_relative "map"
 class Testeuh < Gosu::Window
     def initialize
         super 1280, 720, {resizable: true}
         self.caption = "Gameuh!"
+        @map = Map.new("media/map.txt")
     end
     
     def update
@@ -10,8 +13,9 @@ class Testeuh < Gosu::Window
     end
     
     def draw
-        
+        @map.draw
     end
+
     def needs_cursor?
         true
     end    
---
+++ map.rb
@@ -0,0 +1,45 @@
+
+class Tiles
+    GRASS = 0
+    DIRT = 1
+    DIRTGRASS = 2
+end
+
+class Map
+    attr_reader :width, :height
+    def initialize filename
+        @tileset = Gosu::Image.load_tiles('media/tileset2.png', 60, 60, tileable: true)
+        lines = File.readlines(filename).map { |line| line.chomp }
+
+        @height = lines.size
+        @width = lines.first.size
+        @tiles = Array.new(@width) do |x|
+            Array.new(@height) do |y|
+                case lines[y][x]
+                    when '"'
+                        Tiles::GRASS
+                    when '*'
+                        Tiles::DIRTGRASS
+                    when '#'
+                        Tiles::DIRT
+                    else
+                        nil
+                end
+            end
+        end
+    end
+
+    def draw
+        @height.times do |y|
+            @width.times do |x|
+                value = @tiles[x][y]
+                @tileset[value].draw(x * 50 - 5, y * 50 - 5, 0) if value
+            end
+        end
+    end
+    
+    # Solid at a given pixel position?
+    def solid?(x, y)
+        y < 0 || @tiles[x / 50][y / 50]
+    end
+end
