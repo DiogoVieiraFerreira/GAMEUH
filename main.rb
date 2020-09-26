a3f58480c846fcb8a9a9f4218c010d3b8d873920
Diogo <diogof-6@hotmail.com>
2020-09-26 20:03:19 +0200

    add ennemies on game

--- main.rb
+++ main.rb
@@ -1,22 +1,27 @@
 require 'pp'
 require 'gosu'
 require_relative "map"
-require_relative "vectors"
+require_relative "vector2"
 require_relative "player"
+require_relative "ennemy"
 class Gameuh < Gosu::Window
     def initialize
         super 1280, 720, {resizable: true}
         self.caption = "Gameuh!"
-        @map = Map.new("media/map.txt")
-        
+        @map = Map.new "media/map.txt" 
         @camera_x = @camera_y = 0
-        @player = Player.new(map: @map)
+        @ennemies = @map.ennemies_positions.map do |position|
+            Ennemy.new position, @map
+        end
+        puts @ennemies.count
+        @player = Player.new @map
 
-        @font = Gosu::Font.new(self, Gosu::default_font_name, 20)
+        @font = Gosu::Font.new self, Gosu::default_font_name, 20 
     end
     
     def update
         @player.update
+        @ennemies.each &:update 
         
         # Scrolling follows player
         @camera_x = [[@player.position.x - self.width / 2, 0].max, @map.width * 50 - self.width].min
@@ -27,11 +32,14 @@ class Gameuh < Gosu::Window
         Gosu.translate(-@camera_x, -@camera_y) do
             @map.draw
             draw_rect(@player.position.x, @player.position.y, @player.width, @player.height, Gosu::Color.new(255,0,0))
+            @ennemies.each do |ennemy|
+                draw_rect(ennemy.position.x, ennemy.position.y, ennemy.width,ennemy.height, Gosu::Color.new(150,0,150))
+            end
             @font.draw_text(Gosu.fps, @camera_x, @camera_y, 1, 1, 1, Gosu::Color::WHITE)
         end
     end
 
-    def button_down(id)
+    def button_down id 
         case id
             when Gosu::KB_ESCAPE
                 close
